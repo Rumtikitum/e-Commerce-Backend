71a495b93ace817cbfb72881cc74f85f6a55624e
Richie Tauch <rtauc001@gmail.com>
2021-07-18 19:51:39 -0700

    confirmed that certain requests work for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,16 +7,36 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: [{ model: Product,
+        attributes: ['product_name', 'price', 'stock'],
+      }]
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product,
+        attributes: ['product_name', 'price', 'stock'],
+      }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
